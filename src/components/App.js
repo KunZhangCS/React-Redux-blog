c2862df5b7400bbdf826226c243195568f00d50a
Kun Zhang <kunzhang1988@gmail.com>
2020-03-30 15:39:40 +1000

    Add asynchronous action creator using middleware(redux-thunk)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
-    return <div className="ui container">App</div>;
+    return (
+    <div className="ui container">Post List</div>
+    )
 };
 
 export default App;
